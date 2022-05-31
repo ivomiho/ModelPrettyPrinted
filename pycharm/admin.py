b1656db98837b72e73665baaf8bb8bf9826d59a9
ivomiho <94617231+ivomiho@users.noreply.github.com>
2022-05-31 00:06:23 -0300

    Learn Models

---
+++ pycharm/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Simple, Language, Framework, Movie, Character
+
+admin.site.register(Simple)
+admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Movie)
+admin.site.register(Character)
+
+
---
+++ pycharm/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PycharmConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pycharm'
